3bd32d42d328a6a33068b21dec60e5bb0ad4d334
LucaSartore-USDA <124632444+LucaSartore-USDA@users.noreply.github.com>
2026-09-08 16:18:05 -0400

    Update CRAN compliance handling in mimalloc headers; adjust memory functions and undefine __INTRINSIC_GROUP_WINNT

--- src/mimalloc/include/mimalloc/internal.h
+++ src/mimalloc/include/mimalloc/internal.h
@@ -1126,7 +1126,8 @@ static inline size_t mi_popcount(size_t x) {
 // (AMD Zen3+ (~2020) or Intel Ice Lake+ (~2017). See also issue #201 and pr #253.
 // ---------------------------------------------------------------------------------
 
-#if !MI_TRACK_ENABLED && defined(_WIN32) && (defined(_M_IX86) || defined(_M_X64)) && !MI_CRAN_COMPLIANT
+#if !MI_CRAN_COMPLIANT
+#if !MI_TRACK_ENABLED && defined(_WIN32) && (defined(_M_IX86) || defined(_M_X64))
 #include <intrin.h>
 extern mi_decl_hidden bool _mi_cpu_has_fsrm;
 extern mi_decl_hidden bool _mi_cpu_has_erms;
@@ -1146,6 +1147,14 @@ static inline void _mi_memzero(void* dst, size_t n) {
     memset(dst, 0, n);
   }
 }
+#else
+static inline void _mi_memcpy(void* dst, const void* src, size_t n) {
+  memcpy(dst, src, n);
+}
+static inline void _mi_memzero(void* dst, size_t n) {
+  memset(dst, 0, n);
+}
+#endif
 #else
 static inline void _mi_memcpy(void* dst, const void* src, size_t n) {
   memcpy(dst, src, n);
