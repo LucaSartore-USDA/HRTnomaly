12b2693262ce374ffe10de52f038d0217ff3e074
JustinVanWart-USDA <justin.vanwart@usda.gov>
2026-09-11 06:47:08 -0400

    Fix CRAN portability for R-devel

---
+++ src/allocator.h
@@ -0,0 +1,10 @@
+#ifndef HRTNOMALY_ALLOCATOR_H
+#define HRTNOMALY_ALLOCATOR_H
+
+#include <stdlib.h>
+
+#define mi_malloc malloc
+#define mi_calloc calloc
+#define mi_free free
+
+#endif
--- src/gif.c
+++ src/gif.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include <mimalloc.h>
+#include "allocator.h"
 #include <R.h>
 #include <Rmath.h>
 
